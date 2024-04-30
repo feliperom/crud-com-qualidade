2d8f86a5f448eb5d37337018114cb980f14f3d27
feliperom87 <feliperom87@gmail.com>
2024-04-30 14:21:40 -0300

    Config typescript

--- core/crud.ts
+++ core/crud.ts
@@ -1,9 +1,9 @@
-const fs = require('fs');
+import fs from "fs"; //ES6
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
-function create(content) {
+function create(content: string) {
   // salvar o content no sistema
   fs.writeFileSync(DB_FILE_PATH, content);
   return content;
